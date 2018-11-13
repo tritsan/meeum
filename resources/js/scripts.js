3ad7c262c30b5036d48f4cf06e9b186e874d4eb1
Tristan <tritsan@gmail.com>
2018-11-13 16:05:29 +1100

    added hero content

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -11,9 +11,23 @@ $('.nav-prompt').click(function (){
     } else {
 
         $('.nav-inner').attr('aria-expanded', 'false');
-        
+
         $('.nav-prompt').text('Open Navigation');
 
         }
 });
 
+
+// nav active class
+function activeMenu() {
+
+    var url = window.location.href;
+
+    $('.nav-inner a').filter(function() {
+
+        return this.href == url;
+
+    }).addClass('active');
+}
+
+activeMenu();

c873330ca2b04434ead0b60da8b68b43d5c02621
Tristan <tritsan@gmail.com>
2018-11-13 15:08:15 +1100

    navigation js

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,2 +1,19 @@
 cssVars();
 
+$('.nav-prompt').click(function (){
+
+    if ($('.nav-inner').attr('aria-expanded') == 'false') {
+
+            $('.nav-inner').attr('aria-expanded', 'true');
+
+            $('.nav-prompt').text('Close Navigation');
+
+    } else {
+
+        $('.nav-inner').attr('aria-expanded', 'false');
+        
+        $('.nav-prompt').text('Open Navigation');
+
+        }
+});
+
